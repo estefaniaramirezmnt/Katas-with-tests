d892cc4acadbbddc037a27ce9b5218f1e42efa69
Estefanía <estefaniaramirezmnt@gmail.com>
2024-04-30 16:10:52 +0200

    Added test for nameOfFriends if array is empty or null

--- Katas and tests/nameOfFriends.js
+++ Katas and tests/nameOfFriends.js
@@ -1,6 +1,10 @@
 // Make a list of your friends whose names have exatly 4 letters.
 
 function nameOfFriends(arrayOfFriends) {
+    if (arrayOfFriends === null || arrayOfFriends.length === 0) {
+        return []
+    }
+    
     let listOfFilteredFriends = [];
     for (let firstName of arrayOfFriends) {
         if (firstName.length === 4) {
@@ -10,13 +14,13 @@ function nameOfFriends(arrayOfFriends) {
     return listOfFilteredFriends;
 }
 
+/* 
 function nameOfFriends2(arrayOfFriends) {
     return arrayOfFriends.filter(personName => personName.length === 4);
 }
+*/
 
 var test = ["Ryan", "Kieran", "Jason", "Yous"]
 console.log(nameOfFriends(test))
-console.log(nameOfFriends2(test))
-
 
 module.exports = nameOfFriends;
--- Katas and tests/nameOfFriends.test.js
+++ Katas and tests/nameOfFriends.test.js
@@ -4,3 +4,7 @@ const nameOfFriends = require(`./nameOfFriends`);
 test ('Return only the numbers with 4 letters', () => {
     expect(nameOfFriends(["Ryan", "Kieran", "Jason", "Yous"])).toEqual(["Ryan", "Yous"]);
 })
+
+test ('Return an empty array if the input is null or empty', () => {
+    expect(nameOfFriends([])).toEqual([]);
+})
