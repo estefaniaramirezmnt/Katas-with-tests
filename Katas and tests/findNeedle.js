585ab3485407e4cea1e491ffa2a66b7f157d99d7
Estefanía <estefaniaramirezmnt@gmail.com>
2024-05-02 12:04:33 +0200

    separate functions for array and sentence and created file for test

--- Katas and tests/findNeedle.js
+++ Katas and tests/findNeedle.js
@@ -1,5 +1,23 @@
 // Find the word needle and write in which position it is
 
+function findNeedleInString(sentence) {
+    if (typeof sentence === "string") {
+        sentence = sentence.split(", ");
+        console.log(sentence);
+        // [ 'dog', 'cat', 'needle', 'potato', 'car' ]
+    }
+  return findNeedleInArray(sentence);
+}
+
+function findNeedleInArray(arr) {
+  return `found the needle at position ${arr.indexOf("needle")}`;
+}
+
+var test = ["dog", "cat", "needle", "potato", "car"];
+var test2 = "dog, cat, needle, potato, car";
+
+
+/* 
 function findNeedle(sentence) {
   for (let i = 0; i < sentence.length; i++) {
     if (sentence[i] === "needle") {
@@ -7,5 +25,6 @@ function findNeedle(sentence) {
     }
   }
 }
+*/
 
-module.exports = findNeedle;
+module.exports = { findNeedleInArray: findNeedleInArray, findNeedleInString: findNeedleInString }
